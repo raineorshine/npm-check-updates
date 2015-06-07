49079da3ea5340004df973f49887e8549cd1c5f2
metaraine <raineorshine@gmail.com>
2015-06-06 18:37:27 -0600

    Rewrite upgradeDependencyDeclaration using semver-utils

--- test/testversionmanager.js
+++ test/testversionmanager.js
@@ -8,62 +8,70 @@ describe('Version manager', function () {
 
     describe('upgradeDependencyDeclaration', function () {
         it('numeric upgrades', function () {
-            vm.upgradeDependencyDeclaration("0", "1").should.equal("1");
-            vm.upgradeDependencyDeclaration("1", "10").should.equal("10");
-            vm.upgradeDependencyDeclaration("10", "1").should.equal("1"); // Downgrade
+            vm.upgradeDependencyDeclaration("0", "1.0.0").should.equal("1");
+            vm.upgradeDependencyDeclaration("1", "10.0.0").should.equal("10");
 
-            vm.upgradeDependencyDeclaration("0.1", "1.0").should.equal("1.0");
-            vm.upgradeDependencyDeclaration("1.0", "1.1").should.equal("1.1");
-            vm.upgradeDependencyDeclaration("2.0", "1.1").should.equal("1.1"); // Downgrade
+            vm.upgradeDependencyDeclaration("0.1", "1.0.0").should.equal("1.0");
+            vm.upgradeDependencyDeclaration("1.0", "1.1.0").should.equal("1.1");
 
             vm.upgradeDependencyDeclaration("1.0.0", "1.0.1").should.equal("1.0.1");
             vm.upgradeDependencyDeclaration("1.0.1", "1.1.0").should.equal("1.1.0");
             vm.upgradeDependencyDeclaration("2.0.1", "2.0.11").should.equal("2.0.11");
-            vm.upgradeDependencyDeclaration("2.0.0", "1.0.0").should.equal("1.0.0"); // Downgrade
-
-            vm.upgradeDependencyDeclaration("1.0.0", "1.1").should.equal("1.1");
-            vm.upgradeDependencyDeclaration("1.0.0", "2").should.equal("2");
-            vm.upgradeDependencyDeclaration("22.0.1", "22").should.equal("22"); // Downgrade
         });
 
         it('wildcard upgrades', function () {
-            vm.upgradeDependencyDeclaration("1.x", "1.1").should.equal("1.x");
+            vm.upgradeDependencyDeclaration("1.x", "1.1.0").should.equal("1.x");
             vm.upgradeDependencyDeclaration("1.x.1", "1.1.2").should.equal("1.x.2");
             vm.upgradeDependencyDeclaration("1.0.x", "1.1.1").should.equal("1.1.x");
-            vm.upgradeDependencyDeclaration("1.0.x", "1.1").should.equal("1.1");
-            vm.upgradeDependencyDeclaration("1.0.x", "2").should.equal("2");
-            vm.upgradeDependencyDeclaration("2.0.x", "1.0.0").should.equal("1.0.x"); // Downgrade
+            vm.upgradeDependencyDeclaration("1.0.x", "1.1.0").should.equal("1.1.x");
+            vm.upgradeDependencyDeclaration("1.0.x", "2.0.0").should.equal("2.0.x");
 
-            vm.upgradeDependencyDeclaration("*", "1").should.equal("*");
-            vm.upgradeDependencyDeclaration("*", "1.1").should.equal("*");
-            vm.upgradeDependencyDeclaration("*.1", "2.0.1").should.equal("*.0");
+            vm.upgradeDependencyDeclaration("*", "1.0.0").should.equal("*");
+            vm.upgradeDependencyDeclaration("1.*", "2.0.1").should.equal("2.*");
         });
 
         it('constrained numeric upgrades', function () {
-            vm.upgradeDependencyDeclaration("<1.0", "1.1").should.equal("<1.1");
+            vm.upgradeDependencyDeclaration("<1.0", "1.1.0").should.equal("<1.1");
             vm.upgradeDependencyDeclaration("<1.0", "1.1.1").should.equal("<1.1");
-            vm.upgradeDependencyDeclaration(">=1.0", "2.0").should.equal(">=2.0");
-            vm.upgradeDependencyDeclaration(">= 1.0", "2.0").should.equal(">= 2.0");
+            vm.upgradeDependencyDeclaration(">=1.0", "2.0.0").should.equal(">=2.0");
             vm.upgradeDependencyDeclaration("^1.2.3", "1.2.4").should.equal("^1.2.4");
         });
 
-        it('combined constraints and ranges', function () {
-            vm.upgradeDependencyDeclaration("^1.0.0 < 1.2.0", "3.1.0").should.equal("^3.1.0");
-            vm.upgradeDependencyDeclaration("~1.0 < 1.2.0", "3.1.0").should.equal("~3.0");
+        it('should replace closed ranges with ^', function () {
+            vm.upgradeDependencyDeclaration("1.0.0 < 1.2.0", "3.1.0").should.equal("^3.1.0");
+        });
+        
+        it('should replace multiple ranges with ^ while preserving number of components', function () {
+            vm.upgradeDependencyDeclaration(">1.0 >2.0 < 3.0", "3.1.0").should.equal("^3.1");
+        });
+
+        it('should handle ||', function () {
+            vm.upgradeDependencyDeclaration("~1.0 || ~1.2", "3.1.0").should.equal("~3.1");
+        });
+        
+        it('should use the range with the fewest parts if there are multiple ranges', function () {
+            vm.upgradeDependencyDeclaration("1.1 || 1.2.0", "3.1.0").should.equal("3.1");
+            vm.upgradeDependencyDeclaration("1.2.0 || 1.1", "3.1.0").should.equal("3.1");
+        });
+        
+        it('should preserve wildcards in comparisons', function () {
             vm.upgradeDependencyDeclaration("1.x < 1.2.0", "3.1.0").should.equal("3.x");
-            vm.upgradeDependencyDeclaration("1.2.0 < 1.x", "3.1.0").should.equal("3.x");
+        });
+        
+        it('should use the first operator if a comparison has mixed operators', function () {
+            vm.upgradeDependencyDeclaration("1.x < 1.*", "3.1.0").should.equal("3.x");
         });
 
         it('maintain "unclean" semantic versions', function () {
             vm.upgradeDependencyDeclaration("v1.0", "1.1").should.equal("v1.1");
             vm.upgradeDependencyDeclaration("=v1.0", "1.1").should.equal("=v1.1");
-            vm.upgradeDependencyDeclaration(" =v1.0", "1.1").should.equal(" =v1.1");
+            vm.upgradeDependencyDeclaration(" =v1.0", "1.1").should.equal("=v1.1");
         });
 
         it('maintain "unclean" semantic versions', function () {
             vm.upgradeDependencyDeclaration("v1.0", "1.1").should.equal("v1.1");
             vm.upgradeDependencyDeclaration("=v1.0", "1.1").should.equal("=v1.1");
-            vm.upgradeDependencyDeclaration(" =v1.0", "1.1").should.equal(" =v1.1");
+            vm.upgradeDependencyDeclaration(" =v1.0", "1.1").should.equal("=v1.1");
         });
 
         it('maintain existing version if new version is unknown', function () {
