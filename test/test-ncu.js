3532d6d1af5c2c78eac2e308258c74aedc61b248
metaraine <raineorshine@gmail.com>
2015-08-07 16:07:21 -0600

    Add cli tests for stdin, --jsonAll, and --jsonUpgraded.

--- test/test-ncu.js
+++ test/test-ncu.js
@@ -1,9 +1,13 @@
-var ncu            = require("../lib/npm-check-updates.js");
-var chai           = require("chai");
-var chaiAsPromised = require("chai-as-promised");
-var fs             = require('fs');
+var ncu             = require("../lib/npm-check-updates.js");
+var chai            = require("chai");
+var fs              = require('fs');
+var spawn           = require('spawn-please')
+var BluebirdPromise = require('bluebird')
 
-chai.use(chaiAsPromised);
+chai.use(require("chai-as-promised"));
+chai.use(require('chai-string'))
+
+spawn.Promise = BluebirdPromise;
 
 describe('npm-check-updates', function () {
 
@@ -15,6 +19,33 @@ describe('npm-check-updates', function () {
                     packageData: fs.readFileSync(__dirname + '/ncu/package.json', 'utf-8')
                 })
                 .should.eventually.have.property('express');
+        });
+    });
+
+    describe('cli', function() {
+
+        it('should accept stdin', function() {
+            return spawn('ncu', [], '{ "dependencies": { "express": "1" } }')
+                .then(function (output) {
+                    output.trim().should.startWith('express')
+                });
+        });
+
+        it('should output json with --jsonAll', function() {
+            return spawn('ncu', ['--jsonAll'], '{ "dependencies": { "express": "1" } }')
+                .then(function (output) {
+                  var pkgData = JSON.parse(output);
+                  pkgData.should.have.property('dependencies');
+                  pkgData.dependencies.should.have.property('express');
+                });
+        });
+
+        it('should output only upgraded with --jsonUpgraded', function() {
+            return spawn('ncu', ['--jsonUpgraded'], '{ "dependencies": { "express": "1" } }')
+                .then(function (output) {
+                  var pkgData = JSON.parse(output);
+                  pkgData.should.have.property('express');
+                });
         });
     });
 
