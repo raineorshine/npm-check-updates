raineorshine/npm-check-updates

commit 82acd575311651b79690c359996182b79a85db7a

Upgrade all dependencies with latest versions by default. Add --minimal option for old behavior.

lib/npm-check-updates.js
@@ -177,21 +177,15 @@ function analyzeProjectDependencies(options, pkgData, pkgFile) {
 
     print(options, 'Fetching ' + vm.getVersionTarget(options) + ' versions...', 'verbose');
 
-    return Promise.all([
-        current,
-        vm.upgradePackageDefinitions(current, options)
-    ])
-    .spread(function (current, upgradedAndLatest) {
+    return vm.upgradePackageDefinitions(current, options).spread(function (upgraded, latest) {
 
         var output;
         var newPkgData;
 
-        var upgraded = upgradedAndLatest[0];
-        var latest = upgradedAndLatest[1];
-
         if (options.json) {
+            // use the upgraded dependencies data to generate new package data
             newPkgData = vm.upgradePackageData(pkgData, current, upgraded, latest, options);
-            // don't need try-catch here because pkgData has already been parsed as valid JSON, and vm.upgradePackageData simply does a find-and-replace on that
+            // INVARIANT: we don't need try-catch here because pkgData has already been parsed as valid JSON, and vm.upgradePackageData simply does a find-and-replace on that
             output = options.jsonAll ? jph.parse(newPkgData) :
                 options.jsonDeps ?
                     _.pick(jph.parse(newPkgData), 'dependencies', 'devDependencies', 'optionalDependencies') :
@@ -231,15 +225,13 @@ function printUpgrades(options, args) {
     });
 
     var isSatisfied = _.propertyOf(satisfied);
-    var satisfiedUpgraded = cint.filterObject(args.upgraded, isSatisfied);
-    var unsatisfiedUpgraded = cint.filterObject(args.upgraded, cint.not(isSatisfied));
-    var numSatisfied =   Object.keys(satisfiedUpgraded).length;
-    var numUnsatisfied = Object.keys(unsatisfiedUpgraded).length;
+    var upgraded = options.minimal ? cint.filterObject(args.upgraded, cint.not(isSatisfied)) : args.upgraded;
+    var numUpgraded = Object.keys(upgraded).length;
 
     print(options, '');
 
     // print everything is up-to-date
-    if (numSatisfied === 0 && numUnsatisfied === 0) {
+    if (numUpgraded === 0) {
         var smiley = chalk.green.bold(':)');
         if (options.global) {
             print(options, 'All global packages are up-to-date ' + smiley);
@@ -249,34 +241,19 @@ function printUpgrades(options, args) {
         }
     }
 
-    // print unsatisfied table
-    if (numUnsatisfied > 0) {
-        var unsatisfiedTable = toDependencyTable(options, {
+    // print table
+    if (numUpgraded > 0) {
+        var table = toDependencyTable(options, {
             from: args.current,
-            to: unsatisfiedUpgraded
+            to: upgraded
         }, {
             greatest: options.greatest,
             newest: options.newest
         });
-        print(options, unsatisfiedTable.toString());
+        print(options, table.toString());
     }
 
-    // print satisfied table
-    if (numSatisfied > 0) {
-        var satisfiedTable = toDependencyTable(options, {
-            from: args.current,
-            to: satisfiedUpgraded
-        }, {
-            greatest: options.greatest,
-            newest: options.newest
-        });
-        print(options, (numUnsatisfied > 0 ? '\n' : '') + 'The following dependenc' + (numSatisfied === 1 ? 'y is' : 'ies are') + ' satisfied by ' + (numSatisfied === 1 ? 'its' : 'their') + ' declared version range, but the installed version' + (numSatisfied === 1 ? ' is' : 's are') + ' behind. You can install the latest version' + (numSatisfied === 1 ? '' : 's') + ' without modifying your package file by using ' + chalk.cyan(options.packageManager + ' update') + '. If you want to update the dependenc' + (numSatisfied === 1 ? 'y' : 'ies') + ' in your package file anyway, run ' + chalk.cyan('ncu -a') + '.\n', 'warn');
-        print(options, satisfiedTable.toString(), 'warn');
-    }
-
-    var numToUpgrade = numUnsatisfied + (options.upgradeAll ? numSatisfied : 0);
-
-    if (args.pkgFile && numToUpgrade > 0) {
+    if (args.pkgFile && numUpgraded > 0) {
         if (options.errorLevel >= 2) {
             programError(options, 'Dependencies not up-to-date');
         } else if (args.isUpgrade) {
@@ -286,7 +263,7 @@ function printUpgrades(options, args) {
                     print(options, 'Upgraded ' + args.pkgFile + '\n');
                 });
         } else {
-            print(options, '\nRun ' + chalk.cyan('ncu') + ' with ' + chalk.cyan(numUnsatisfied > 0 ? '-u' : '-a') + ' to upgrade ' + getPackageFileName(options));
+            print(options, '\nRun ' + chalk.cyan('ncu') + ' with ' + chalk.cyan('-u') + ' to upgrade ' + getPackageFileName(options));
         }
     }
 
@@ -302,10 +279,9 @@ function printUpgrades(options, args) {
 function initOptions(options) {
 
     return _.assign({}, options, {
-        // 'upgradeAll' is a type of an upgrade so if it's set, we set 'upgrade' as well
-        upgrade: options.upgrade || options.upgradeAll,
         // convert silent option to loglevel silent
         loglevel: options.silent ? 'silent' : options.loglevel,
+        minimal: options.minimal === undefined ? false : options.loglevel,
         // add shortcut for any keys that start with 'json'
         json: _(options)
             .keys()
@@ -420,8 +396,6 @@ function run(opts) {
             // if they want to modify the package file, we must disable jsonUpgraded
             // otherwise the write operation will not happen
             jsonUpgraded: !options.upgrade,
-            // should not suggest upgrades to versions within the specified version range if upgradeAll is explicitly set to false. Will become the default in the next major version.
-            upgradeAll: options.upgradeAll === undefined ? true : options.upgradeAll,
             loglevel: 'silent',
             args: []
         });

bin/ncu
@@ -22,6 +22,7 @@ program
     .option('--jsonUpgraded', 'output upgraded dependencies in json')
     .option('-l, --loglevel <n>', 'what level of logs to report: silent, error, minimal, warn, info, verbose, silly (default: warn)', 'warn')
     .option('-m, --packageManager <name>', 'npm (default) or bower', 'npm')
+    .option('--minimal', 'do not upgrade newer versions that are already satisfied by the version range according to semver')
     .option('-n, --newest', 'find the newest versions available instead of the latest stable versions')
     .option('-o, --optional', 'check only optionalDependencies')
     .option('--packageData', 'include stringified package file (use stdin instead)')
@@ -34,7 +35,6 @@ program
     .option('--timeout <ms>', 'a global timeout in ms')
     .option('-u, --upgrade', 'overwrite package file')
     .option('-x, --reject <matches>', 'exclude packages matching the given string, comma-delimited list, or regex')
-    .option('-a, --upgradeAll', 'include even those dependencies whose latest version satisfies the declared semver dependency')
     .option('--semverLevel <level>', 'find the highest version within "major" or "minor"')
     .option('--removeRange', 'remove version ranges from the final package version')
     .option('-v, --version', pkg.version, function() { console.log(pkg.version); process.exit(0); })

lib/versionmanager.js
@@ -212,6 +212,7 @@ function filterAndReject(filter, reject) {
     );
 }
 
+/** Returns an 2-tuple of upgradedDependencies and their latest versions */
 function upgradePackageDefinitions(currentDependencies, options) {
     var versionTarget = getVersionTarget(options);
 
@@ -225,7 +226,7 @@ function upgradePackageDefinitions(currentDependencies, options) {
         });
 
         var filteredUpgradedDependencies = _.pickBy(upgradedDependencies, function (v, dep) {
-            return !options.jsonUpgraded || options.upgradeAll || !isSatisfied(latestVersions[dep], currentDependencies[dep]);
+            return !options.jsonUpgraded || !options.minimal || !isSatisfied(latestVersions[dep], currentDependencies[dep]);
         });
 
         return [filteredUpgradedDependencies, latestVersions];
@@ -245,7 +246,7 @@ function upgradePackageData(pkgData, oldDependencies, newDependencies, newVersio
     options = options || {};
 
     for (var dependency in newDependencies) {
-        if (options.upgradeAll || !isSatisfied(newVersions[dependency], oldDependencies[dependency])) {
+        if (!options.minimal || !isSatisfied(newVersions[dependency], oldDependencies[dependency])) {
             var expression = '"' + dependency + '"\\s*:\\s*"' + escapeRegexp(oldDependencies[dependency] + '"');
             var regExp = new RegExp(expression, 'g');
             pkgData = pkgData.replace(regExp, '"' + dependency + '": ' + '"' + newDependencies[dependency] + '"');

test/test-ncu.js
@@ -43,7 +43,7 @@ describe('npm-check-updates', function () {
             ]);
         });
 
-        it('should suggest upgrades to versions within the specified version range if jsonUpraded is true and upgradeAll is not given (backwards compatible behavior until next version)', function () {
+        it('should suggest upgrades to versions within the specified version range if jsonUpraded is true', function () {
             var upgraded = ncu.run({
                 // juggernaut has been deprecated at v2.1.1 so it is unlikely to invalidate this test
                 packageData: '{ "dependencies": { "juggernaut": "^2.1.0" } }',
@@ -58,29 +58,15 @@ describe('npm-check-updates', function () {
             ]);
         });
 
-        it('should not suggest upgrades to versions within the specified version range if jsonUpraded is true and upgradeAll is explicitly set to false', function () {
+        it('should not suggest upgrades to versions within the specified version range if jsonUpraded is true and minimial is true', function () {
             var upgraded = ncu.run({
                 // juggernaut has been deprecated at v2.1.1 so it is unlikely to invalidate this test
                 packageData: '{ "dependencies": { "juggernaut": "^2.1.0" } }',
                 jsonUpgraded: true,
-                upgradeAll: false
+                minimal: true
             });
 
             return upgraded.should.eventually.not.have.property('juggernaut');
-        });
-
-        it('should ignore newer packages that satisfy the declared version range if they are installed in node_modules', function () {
-
-            var upgraded = ncu.run({
-                // { "dependencies": { "escape-string-regexp": "^1.0.4" } }
-                // latest is 1.0.5
-                packageFile: 'test/test-modules/package.json',
-                packageFileDir: true, // appears to be redundant with upgradeAll in this test case, but it's already built so I give up :(. Too much effort to satisfy an edge case (#201).
-                jsonUpgraded: true,
-                upgradeAll: false
-            });
-
-            return upgraded.should.eventually.not.have.property('escape-string-regexp');
         });
     });
 

test/test-versionmanager.js
@@ -134,13 +134,13 @@ describe('versionmanager', function () {
             'mocha': '2.2.5'
         };
 
-        it('should upgrade the dependencies in the given package data (except for satisfied)', function () {
+        it('should upgrade the dependencies in the given package data (including satisfied)', function () {
             JSON.parse(vm.upgradePackageData(pkgData, oldDependencies, newDependencies, newVersions))
                 .should.eql({
                     'name': 'npm-check-updates',
                     'dependencies': {
                         'bluebird': '^2.9',
-                        'bindings': '^1.1.0'
+                        'bindings': '^1.2.1'
                     },
                     'devDependencies': {
                         'mocha': '^2'
@@ -148,13 +148,13 @@ describe('versionmanager', function () {
                 });
         });
 
-        it('should upgrade the dependencies in the given package data (including satisfied)', function () {
-            JSON.parse(vm.upgradePackageData(pkgData, oldDependencies, newDependencies, newVersions, {upgradeAll: true}))
+        it('should upgrade the dependencies in the given package data (except for satisfied)', function () {
+            JSON.parse(vm.upgradePackageData(pkgData, oldDependencies, newDependencies, newVersions, {minimal: true}))
                 .should.eql({
                     'name': 'npm-check-updates',
                     'dependencies': {
                         'bluebird': '^2.9',
-                        'bindings': '^1.2.1'
+                        'bindings': '^1.1.0'
                     },
                     'devDependencies': {
                         'mocha': '^2'

commit 5abc4adfa38f72467715f553e60f8823a89128fa

Fix --minimal initialization. Broken from commit 82acd575.

lib/npm-check-updates.js
@@ -302,7 +302,7 @@ function initOptions(options) {
         filter: options.args.join(' ') || options.filter,
         // convert silent option to loglevel silent
         loglevel: options.silent ? 'silent' : options.loglevel,
-        minimal: options.minimal === undefined ? false : options.loglevel,
+        minimal: options.minimal === undefined ? false : options.minimal,
         // add shortcut for any keys that start with 'json'
         json: _(options)
             .keys()