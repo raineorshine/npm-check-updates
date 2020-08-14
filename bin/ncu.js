fb357d3c1cb1462a1919515050a46d8989cfbba1
Raine Revere <raine@cybersemics.org>
2020-08-14 16:29:23 -0600

    Refactor CLI options.

--- bin/ncu.js
+++ bin/ncu.js
@@ -7,6 +7,7 @@ const updateNotifier = require('update-notifier')
 const _ = require('lodash')
 const ncu = require('../lib/npm-check-updates')
 const pkg = require('../package.json')
+const { cliOptions } = require('../lib/constants')
 
 // check if a new version of ncu is available and print an update notification
 const notifier = updateNotifier({ pkg })
@@ -14,40 +15,18 @@ if (notifier.update && notifier.update.latest !== pkg.version) {
   notifier.notify({ defer: false, isGlobal: true })
 }
 
+// start commander program
 program
   .description('[filter] is a list or regex of package names to check (all others will be ignored).')
   .usage('[options] [filter]')
-  .option('--concurrency <n>', 'max number of concurrent HTTP requests to registry.', s => parseInt(s, 10), 8)
-  .option('--configFilePath <path>', 'rc config file path (default: directory of `packageFile` or ./ otherwise)')
-  .option('--configFileName <path>', 'rc config file name (default: .ncurc.{json,yml,js})')
-  .option('--cwd <path>', 'working directory in which npm will be executed')
-  .option('--dep <dep>', 'check only a specific section(s) of dependencies: prod, dev, peer, optional, bundle (comma-delimited)')
-  .option('-e, --error-level <n>', 'set the error-level. 1: exits with error code 0 if no errors occur. 2: exits with error code 0 if no packages need updating (useful for continuous integration).', s => parseInt(s, 10), 1)
-  .option('--engines-node', 'include only packages that satisfy engines.node as specified in the package file')
-  .option('-f, --filter <matches>', 'include only package names matching the given string, comma-or-space-delimited list, or /regex/')
-  .option('-g, --global', 'check global packages instead of in the current project')
-// program.json is set to true in programInit if any options that begin with 'json' are true
-  .option('-i, --interactive', 'Enable interactive prompts for each dependency; implies -u unless one of the json options are set')
-  .option('-j, --jsonAll', 'output new package file instead of human-readable message')
-  .option('--jsonDeps', 'Will return output like `jsonAll` but only lists `dependencies`, `devDependencies`, and `optionalDependencies` of the new package data.')
-  .option('--jsonUpgraded', 'output upgraded dependencies in json')
-  .option('-l, --loglevel <n>', 'what level of logs to report: silent, error, minimal, warn, info, verbose, silly', 'warn')
-  .option('-m, --minimal', 'do not upgrade newer versions that are already satisfied by the version range according to semver')
-  .option('-n, --newest', 'find the newest versions available instead of the latest stable versions')
-  .option('-p, --packageManager <name>', 'npm, yarn', 'npm')
-  .option('--packageData', 'include stringified package file (use stdin instead)')
-  .option('--packageFile <filename>', 'package file location (default: ./package.json)')
-  .option('--pre <n>', 'Include -alpha, -beta, -rc. (default: 0; default with --newest and --greatest: 1)')
-  .option('--prefix <path>', 'Used as current working directory in npm')
-  .option('-r, --registry <url>', 'specify third-party npm registry')
-  .option('--removeRange', 'remove version ranges from the final package version')
-  .option('-s, --silent', 'don\'t output anything (--loglevel silent)')
-  .option('--semverLevel <level>', 'find the highest version within "major" or "minor"')
-  .option('-t, --greatest', 'find the highest versions available instead of the latest stable versions')
-  .option('--timeout <ms>', 'global timeout in milliseconds. (default: no global timeout and 30 seconds per npm-registery-fetch)')
-  .option('-u, --upgrade', 'overwrite package file')
-  .option('-x, --reject <matches>', 'exclude packages matching the given string, comma-or-space-delimited list, or /regex/')
-  .version(pkg.version)
+
+// add cli options
+cliOptions.forEach(({ name, description, default: defaultValue, parse }) =>
+  // handle 3rd/4th argument polymorphism
+  program.option(name, description, parse || defaultValue, parse ? defaultValue : undefined))
+
+// set version option at the end
+program.version(pkg.version)
 
 program.parse(process.argv)
 
