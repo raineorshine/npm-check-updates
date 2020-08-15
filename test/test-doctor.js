a3e874a96df5abdb09d3ae97908f3808301a1dc5
Raine Revere <raine@cybersemics.org>
2020-08-15 17:57:19 -0600

    doctor: basic failure case

--- test/test-doctor.js
+++ test/test-doctor.js
@@ -3,34 +3,25 @@
 const fs = require('fs')
 const path = require('path')
 const chai = require('chai')
-const chalk = require('chalk')
 const chaiAsPromised = require('chai-as-promised')
 const spawn = require('spawn-please')
 const rimraf = require('rimraf')
 
-const should = chai.should()
+chai.should()
 chai.use(chaiAsPromised)
 
-describe.only('doctor', () => {
+describe('doctor', () => {
 
   it('throw an error if there is no package file', async () => {
-
     const cwd = path.join(__dirname, 'doctor/nopackagefile')
-    const pkgPath = path.join(cwd, 'package.json')
-
     return spawn('node', [path.join(__dirname, '../bin/cli.js'), '--doctor'], { cwd })
       .should.eventually.be.rejectedWith('Missing or invalid package.json')
-
   })
 
   it('throw an error if there is no test script', async () => {
-
     const cwd = path.join(__dirname, 'doctor/notestscript')
-    const pkgPath = path.join(cwd, 'package.json')
-
     return spawn('node', [path.join(__dirname, '../bin/cli.js'), '--doctor'], { cwd })
       .should.eventually.be.rejectedWith('No npm "test" script')
-
   })
 
   it('throw an error if --packageData or --packageFile are supplied', async () => {
@@ -44,20 +35,36 @@ describe.only('doctor', () => {
 
   })
 
-  it('use npm install to restore lockfile if missing', async () => {
+  it('identify broken upgrade', async function() {
 
-    const cwd = path.join(__dirname, 'doctor/nolockfile')
+    this.timeout(30000)
+
+    const cwd = path.join(__dirname, 'doctor/fail')
+    const pkgPath = path.join(cwd, 'package.json')
     const lockfilePath = path.join(cwd, 'package-lock.json')
     const nodeModulesPath = path.join(cwd, 'node_modules')
+    const pkgOriginal = fs.readFileSync(path.join(cwd, 'package-original.json'), 'utf-8')
+    let stdout = ''
+    let stderr = ''
 
-    try
-    {
-      fs.existsSync(lockfilePath).should.equal(false)
-      const output = await spawn('node', [path.join(__dirname, '../bin/cli.js'), '--doctor'], { cwd })
-      output.should.include('No package-lock.json found')
-      fs.existsSync(lockfilePath).should.equal(true)
+    try {
+      await spawn('node', [path.join(__dirname, '../bin/cli.js'), '--doctor'], {
+        cwd,
+        stdout: function(data) {
+          stdout += data
+        },
+        stderr: function(data) {
+          stderr += data
+        },
+      })
+    }
+    catch (e) {
+      stdout.should.include('✓ ncu-test-v2 1.0.0 → 2.0.0')
+      stdout.should.include('Breaks with v2.x')
+      stderr.should.include('✗ ncu-test-return-version 1.0.0 → 2.0.0')
     }
     finally {
+      fs.writeFileSync(pkgPath, pkgOriginal)
       fs.unlinkSync(lockfilePath)
       rimraf.sync(nodeModulesPath)
     }
