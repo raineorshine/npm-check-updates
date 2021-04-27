bfd6e157b7bee402cc130a9f8341380cdb26fe25
korelstar <korelstar@users.noreply.github.com>
2021-04-27 18:02:18 +0200

    print summary of incompatible/ignored updates

--- test/versionmanager.test.js
+++ test/versionmanager.test.js
@@ -329,6 +329,31 @@ describe('versionmanager', () => {
     })
   })
 
+  describe('getIgnoredUpgrades', function () {
+    it('ncu-test-peer-update', async () => {
+      const data = await vm.getIgnoredUpgrades({
+        'ncu-test-return-version': '1.0.0',
+        'ncu-test-peer': '1.0.0',
+      }, {
+        'ncu-test-return-version': '1.1.0',
+        'ncu-test-peer': '1.1.0',
+      }, {
+        'ncu-test-peer': {
+          'ncu-test-return-version': '1.1.x'
+        }
+      }, {})
+      data.should.deep.equal({
+        'ncu-test-return-version': {
+          from: '1.0.0',
+          to: '2.0.0',
+          reason: {
+            'ncu-test-peer': '1.1.x'
+          }
+        }
+      })
+    })
+  })
+
   describe('getPeerDependenciesFromRegistry', function () {
     it('single package', async () => {
       const data = await vm.getPeerDependenciesFromRegistry({ 'ncu-test-peer': '1.0' }, {})
