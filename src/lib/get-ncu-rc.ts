06f029bd19db5a7da90e1b446b4dfd9d3dbcf652
Raine Revere <raine@cybersemics.org>
2021-05-02 13:23:20 -0600

    Convert index and getNcurc to typescript.

---
+++ src/lib/get-ncu-rc.ts
@@ -0,0 +1,36 @@
+import path from 'path'
+import _ from 'lodash'
+import { rcFile } from 'rc-config-loader'
+
+interface Options {
+  configFileName?: string,
+  configFilePath?: string,
+  packageFile?: string,
+}
+
+/**
+ * Loads the .ncurc config file.
+ *
+ * @param [cfg]
+ * @param [cfg.configFileName=.ncurc]
+ * @param [cfg.configFilePath]
+ * @param [cfg.packageFile]
+ * @returns
+ */
+export default function getNcurc({ configFileName, configFilePath, packageFile }: Options = {}) {
+
+  const result = rcFile('ncurc', {
+    configFileName: configFileName || '.ncurc',
+    defaultExtension: ['.json', '.yml', '.js'],
+    cwd: configFilePath ||
+      (packageFile ? path.dirname(packageFile) : undefined)
+  })
+
+  // flatten config object into command line arguments to be read by commander
+  const args = result ?
+    _.flatten(_.map(result.config, (value, name) =>
+      value === true ? [`--${name}`] : [`--${name}`, value]
+    )) : []
+
+  return result ? { ...result, args } : null
+}
--- src/lib/mergeOptions.ts
+++ src/lib/mergeOptions.ts
@@ -1,3 +1,7 @@
+import { Options } from '../types'
+
+type OptionKey = keyof Options
+
 /** Merges two arrays into one, removing duplicates. */
 function mergeArrays(arr1: any[], arr2: any[]) {
   return Array.from(new Set([...arr1 || [], ...arr2 || []]))
@@ -7,13 +11,13 @@ function mergeArrays(arr1: any[], arr2: any[]) {
  * Shallow merge (specific or all) properties.
  * If some properties both are arrays, then merge them also.
  */
-function mergeOptions(options1: any, options2: any) {
+function mergeOptions(options1: Options, options2: Options) {
   options1 = options1 || {}
   options2 = options2 || {}
   const result = { ...options1, ...options2 }
-  Object.keys(result).forEach(key => {
+  ;(Object.keys(result) as OptionKey[]).forEach(key => {
     if (Array.isArray(options1[key]) && Array.isArray(options2[key])) {
-      result[key] = mergeArrays(options1[key], options2[key])
+      result[key] = mergeArrays(options1[key] as any[], options2[key] as any[]) as any
     }
   })
   return result
