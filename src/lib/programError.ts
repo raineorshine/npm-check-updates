3f11a1cdd2b173436f8c8e45a373dbb0d2e301cb
Raine Revere <raine@cybersemics.org>
2021-05-28 16:20:34 -0600

    Move findPackage, getPackageFileName, and programError to /src/lib

---
+++ src/lib/programError.ts
@@ -0,0 +1,17 @@
+import _ from 'lodash'
+import cint from 'cint'
+import { print } from '../logging'
+import { Options } from '../types'
+
+/** Print an error. Exit the process if in CLI mode. */
+function programError(options: Options, message: string) {
+  if (options.cli) {
+    print(options, message, null, 'error')
+    process.exit(1)
+  }
+  else {
+    throw new Error(message)
+  }
+}
+
+export default programError
