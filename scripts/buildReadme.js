10317ef5fbb764737890967c37364c60cc7de68a
Raine Revere <raine@cybersemics.org>
2021-05-29 08:17:42 -0600

    Convert cli-options to Typescript.

--- scripts/buildReadme.js
+++ scripts/buildReadme.js
@@ -5,7 +5,7 @@
 const fs = require('fs')
 const path = require('path')
 const spawn = require('spawn-please')
-const cliOptions = require('../build/src/cli-options.js')
+const cliOptions = require('../build/src/cli-options.js').default
 
 /** Escape closing block comments that would interfere with JSDOC. */
 const escapeComments = s => s.replace(/\*\*\//g, '**\\/')
