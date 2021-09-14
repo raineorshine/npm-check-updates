199beced6574bcc7983d9a7a11379237d548ed26
Nihal Gonsalves <nihal@nihalgonsalves.com>
2021-09-14 11:40:14 +0200

    Fix typo in `registery`

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -197,7 +197,7 @@ declare namespace ncu {
     target?: string;
 
     /**
-     * Global timeout in milliseconds. (default: no global timeout and 30 seconds per npm-registery-fetch).
+     * Global timeout in milliseconds. (default: no global timeout and 30 seconds per npm-registry-fetch).
      */
     timeout?: number;
 
