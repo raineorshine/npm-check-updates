23e25d08cbbe030252422e5b92167fadf8cdfc6b
XhmikosR <xhmikosr@gmail.com>
2021-11-06 08:51:03 +0200

    Update dependencies
    Mostly patch and minor dependencies except for `@types/node`, `mocha` and `@types/mocha`.

--- .ncurc.js
+++ .ncurc.js
@@ -8,9 +8,9 @@ module.exports = {
 
     // "Must use import to load ES Module"
     // These can be removed once the tests are converted to Typescript
+    'find-up',
     'get-stdin',
     'globby',
-    'mocha',
     'p-map',
     'remote-git-tags',
     'strip-ansi',
