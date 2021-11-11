f35cdf47c39858822f57cec77f3fbed54a4b5a83
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2021-11-11 21:02:42 +0000

    tests: migrate cli-options

--- test/cli-options.test.ts
+++ test/cli-options.test.ts
@@ -1,14 +1,12 @@
-'use strict'
-
-const chai = require('chai')
-const cliOptions = require('../src/cli-options').default
+import chai from 'chai'
+import cliOptionsSorted from '../src/cli-options'
 
 chai.should()
 
 describe('cli-options', () => {
 
   it('require long and description properties', () => {
-    cliOptions.forEach(option => {
+    cliOptionsSorted.forEach(option => {
       option.should.have.property('long')
       option.should.have.property('description')
     })
