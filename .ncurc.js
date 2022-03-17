7b47ef3e005b0e97d8fcc9f8cf8eef5d8095f17e
Chris Midgley <chris.midgley@newredo.com>
2022-03-17 13:25:45 +0000

    fix: allow updating to eslint 8 (#1071)
    Co-authored-by: Raine Revere <raine@cybersemics.org>

--- .ncurc.js
+++ .ncurc.js
@@ -1,14 +1,9 @@
 module.exports = {
   reject: [
-    // "If you use TypeScript, you will want to stay on Chalk 4 until TypeScript 4.6 is out."
+    // ESM only modules
     // https://github.com/microsoft/TypeScript/issues/46452
     'chalk',
     'cint',
-    // eslint-plugin-promise does not support eslint v8 yet
-    // https://github.com/xjamundx/eslint-plugin-promise/pull/219
-    'eslint',
-    // "Must use import to load ES Module"
-    // These can be removed once the tests are converted to Typescript
     'find-up',
     'get-stdin',
     'globby',
