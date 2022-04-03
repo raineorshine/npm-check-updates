57e15a5715de8e757a6d77ff461d365c52d2f83a
Raine Revere <raine@cybersemics.org>
2022-04-03 07:52:18 -0600

    Add better error handling for unknown rc file options (#1085).

--- src/lib/getNcuRc.ts
+++ src/lib/getNcuRc.ts
@@ -1,12 +1,21 @@
 import path from 'path'
 import _ from 'lodash'
 import { rcFile } from 'rc-config-loader'
+import cliOptions, { CLIOption } from '../cli-options'
+import { Index } from '../types'
 
 interface Options {
   configFileName?: string,
   configFilePath?: string,
   packageFile?: string,
 }
+
+// put cliOptions into an object for O(1) lookups
+const cliOptionMap = cliOptions.reduce((accum, option) => ({
+  ...accum,
+  ...option.short ? { [option.short]: option } : null,
+  ...option.long ? { [option.long]: option } : null,
+}), {} as Index<CLIOption>)
 
 /**
  * Loads the .ncurc config file.
@@ -25,6 +34,12 @@ function getNcuRc({ configFileName, configFilePath, packageFile }: Options = {})
     cwd: configFilePath ||
       (packageFile ? path.dirname(packageFile) : undefined)
   })
+
+  // validate arguments here to provide a better error message
+  const unknownOptions = Object.keys(result?.config || {}).filter(arg => !cliOptionMap[arg])
+  if (unknownOptions.length > 0) {
+    console.error(`Unknown option${unknownOptions.length === 1 ? '' : 's'} found in rc file:`, unknownOptions.join(', '))
+  }
 
   // flatten config object into command line arguments to be read by commander
   const args = result ?
