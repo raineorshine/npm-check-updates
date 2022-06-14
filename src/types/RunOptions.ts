d991e22c89efaa444adcc7fe88ce2506ced09da1
Raine Revere <raine@cybersemics.org>
2022-06-14 08:55:56 -0600

    Clean up RunOptions type.

--- src/types/RunOptions.ts
+++ src/types/RunOptions.ts
@@ -1,201 +1,123 @@
-import { FilterRejectPattern } from './FilterRejectPattern'
+import { FilterFunction } from './FilterFunction'
 import { PackageFile } from './PackageFile'
-import { Target } from './Target'
+import { TargetFunction } from './TargetFunction'
 
 /** Options that can be given on the CLI or passed to the ncu module to control all behavior. */
 export interface RunOptions {
-  /**
-   * Force color in terminal
-   */
+  /** Force color in terminal */
   color?: boolean
 
-  /**
-   * Max number of concurrent HTTP requests to registry. (default: 8)
-   */
+  /** Max number of concurrent HTTP requests to registry. (default: 8) */
   concurrency?: number
 
-  /**
-   * Config file name. (default: .ncurc.{json,yml,js})
-   */
+  /** Config file name. (default: .ncurc.{json,yml,js}) */
   configFileName?: string
 
-  /**
-   * Directory of .ncurc config file. (default: directory of `packageFile`)
-   */
+  /** Directory of .ncurc config file. (default: directory of `packageFile`) */
   configFilePath?: string
 
-  /**
-   * Working directory in which npm will be executed.
-   */
+  /** Working directory in which npm will be executed. */
   cwd?: string
 
-  /**
-   * Run recursively in current working directory. Alias of (--packageFile '**\/package.json').
-   */
+  /** Run recursively in current working directory. Alias of (--packageFile '**\/package.json'). */
   deep?: boolean
 
-  /**
-   * Check one or more sections of dependencies only: dev, optional, peer, prod, bundle (comma-delimited). (default: "prod,dev,bundle,optional")
-   */
+  /** Check one or more sections of dependencies only: dev, optional, peer, prod, bundle (comma-delimited). (default: "prod,dev,bundle,optional") */
   dep?: string
 
-  /**
-   * Include deprecated packages.
-   */
+  /** Include deprecated packages. */
   deprecated?: boolean
 
-  /**
-   * Iteratively installs upgrades and runs tests to identify breaking upgrades. Run "ncu --doctor" for detailed help. Add "-u" to execute.
-   */
+  /** Iteratively installs upgrades and runs tests to identify breaking upgrades. Run "ncu --doctor" for detailed help. Add "-u" to execute. */
   doctor?: boolean
 
-  /**
-   * Specifies the install script to use in doctor mode. (default: npm install/yarn)
-   */
+  /** Specifies the install script to use in doctor mode. (default: npm install/yarn) */
   doctorInstall?: string
 
-  /**
-   * Specifies the test script to use in doctor mode. (default: npm test)
-   */
+  /** Specifies the test script to use in doctor mode. (default: npm test) */
   doctorTest?: string
 
-  /**
-   * Include only packages that satisfy engines.node as specified in the package file.
-   */
+  /** Include only packages that satisfy engines.node as specified in the package file. */
   enginesNode?: boolean
 
-  /**
-   * Set the error level. 1: exits with error code 0 if no errors occur. 2: exits with error code 0 if no packages need updating (useful for continuous integration). (default: 1)
-   */
+  /** Set the error level. 1: exits with error code 0 if no errors occur. 2: exits with error code 0 if no packages need updating (useful for continuous integration). (default: 1) */
   errorLevel?: number
 
-  /**
-   * Include only package names matching the given string, wildcard, glob, comma-or-space-delimited list, /regex/ or function that returns true.
-   */
-  filter?: FilterRejectPattern
+  /** Include only package names matching the given string, wildcard, glob, comma-or-space-delimited list, /regex/ or function that returns true. */
+  filter?: string | string[] | RegExp | RegExp[] | FilterFunction
 
-  /**
-   * Filter on package version using comma-or-space-delimited list, /regex/ or function that returns true.
-   */
-  filterVersion?: FilterRejectPattern
+  /** Filter on package version using comma-or-space-delimited list, /regex/ or function that returns true. */
+  filterVersion?: string | string[] | RegExp | RegExp[] | FilterFunction
 
-  /**
-   * Enable additional output data, string or comma-delimited list: ownerChanged, repo. ownerChanged: shows if the package owner changed between versions. repo: infers and displays links to source code repository. (default: [])
-   */
+  /** Enable additional output data, string or comma-delimited list: ownerChanged, repo. ownerChanged: shows if the package owner changed between versions. repo: infers and displays links to source code repository. (default: []) */
   format?: string[]
 
-  /**
-   * Check global packages instead of in the current project.
-   */
+  /** Check global packages instead of in the current project. */
   global?: boolean
 
-  /**
-   * Enable interactive prompts for each dependency, implies -u unless one of the json options are set,
-   */
+  /** Enable interactive prompts for each dependency, implies -u unless one of the json options are set, */
   interactive?: boolean
 
-  /**
-   * Output new package file instead of human-readable message.
-   */
+  /** Output new package file instead of human-readable message. */
   jsonAll?: boolean
 
-  /**
-   * Like `jsonAll` but only lists `dependencies`, `devDependencies`, `optionalDependencies`, etc of the new package data.
-   */
+  /** Like `jsonAll` but only lists `dependencies`, `devDependencies`, `optionalDependencies`, etc of the new package data. */
   jsonDeps?: boolean
 
-  /**
-   * Output upgraded dependencies in json.
-   */
+  /** Output upgraded dependencies in json. */
   jsonUpgraded?: boolean
 
-  /**
-   * Amount to log: silent, error, minimal, warn, info, verbose, silly. (default: "warn")
-   */
+  /** Amount to log: silent, error, minimal, warn, info, verbose, silly. (default: "warn") */
   loglevel?: string
 
-  /**
-   * Merges nested configs with the root config file for --deep or --packageFile options. (default: false)
-   */
+  /** Merges nested configs with the root config file for --deep or --packageFile options. (default: false) */
   mergeConfig?: boolean
 
-  /**
-   * Do not upgrade newer versions that are already satisfied by the version range according to semver.
-   */
+  /** Do not upgrade newer versions that are already satisfied by the version range according to semver. */
   minimal?: boolean
 
-  /**
-   * Package file data (you can also use stdin).
-   */
+  /** Package file data (you can also use stdin). */
   packageData?: string | PackageFile
 
-  /**
-   * Package file(s) location. (default: ./package.json)
-   */
+  /** Package file(s) location. (default: ./package.json) */
   packageFile?: string
 
-  /**
-   * npm, yarn (default: "npm")
-   */
+  /** npm, yarn (default: "npm") */
   packageManager?: string
 
-  /**
-   * Check peer dependencies of installed packages and filter updates to compatible versions. Run "ncu --help --peer" for details.
-   */
+  /** Check peer dependencies of installed packages and filter updates to compatible versions. Run "ncu --help --peer" for details. */
   peer?: boolean
 
-  /**
-   * Include -alpha, -beta, -rc. (default: 0; default with --newest and --greatest: 1)
-   */
+  /** Include -alpha, -beta, -rc. (default: 0; default with --newest and --greatest: 1) */
   pre?: boolean
 
-  /**
-   * Current working directory of npm.
-   */
+  /** Current working directory of npm. */
   prefix?: string
 
-  /**
-   * Third-party npm registry.
-   */
+  /** Third-party npm registry. */
   registry?: string
 
-  /**
-   * Exclude packages matching the given string, wildcard, glob, comma-or-space-delimited list, /regex/ or function that returns true.
-   */
-  reject?: FilterRejectPattern
+  /** Exclude packages matching the given string, wildcard, glob, comma-or-space-delimited list, /regex/ or function that returns true. */
+  reject?: string | string[] | RegExp | RegExp[] | FilterFunction
 
-  /**
-   * Exclude package.json versions using comma-or-space-delimited list, /regex/ or function that returns true.
-   */
-  rejectVersion?: FilterRejectPattern
+  /** Exclude package.json versions using comma-or-space-delimited list, /regex/ or function that returns true. */
+  rejectVersion?: string | string[] | RegExp | RegExp[] | FilterFunction
 
-  /**
-   * Remove version ranges from the final package version.
-   */
+  /** Remove version ranges from the final package version. */
   removeRange?: boolean
 
-  /**
-   * Number of times to retry failed requests for package info. (default: 3)
-   */
+  /** Number of times to retry failed requests for package info. (default: 3) */
   retry?: number
 
-  /**
-   * Don't output anything (--loglevel silent).
-   */
+  /** Don't output anything (--loglevel silent). */
   silent?: boolean
 
-  /**
-   * Target version or function that returns version to upgrade to: latest, newest, greatest, minor, patch. Run "ncu --help --target" for details. (default: "latest")
-   */
-  target?: Target
+  /** Target version or function that returns version to upgrade to: latest, newest, greatest, minor, patch. Run "ncu --help --target" for details. (default: "latest") */
+  target?: 'latest' | 'newest' | 'greatest' | 'minor' | 'patch' | TargetFunction
 
-  /**
-   * Global timeout in milliseconds. (default: no global timeout and 30 seconds per npm-registry-fetch)
-   */
+  /** Global timeout in milliseconds. (default: no global timeout and 30 seconds per npm-registry-fetch) */
   timeout?: number
 
-  /**
-   * Overwrite package file with upgraded versions instead of just outputting to console.
-   */
+  /** Overwrite package file with upgraded versions instead of just outputting to console. */
   upgrade?: boolean
 }
