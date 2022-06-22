8dd50730a15d4dd05e557a0c043ca1fa9d2d7719
Amy Grouse <amygrouse@Amys-MacBook-Pro-5.local>
2022-06-22 15:52:42 -0400

    fixed no mutations lint issue

--- src/package-managers/staticRegistry.ts
+++ src/package-managers/staticRegistry.ts
@@ -4,20 +4,31 @@ import { Version } from '../types/Version'
 import { Options } from '../types/Options'
 import { StaticRegistry } from '../types/StaticRegistry'
 
-const registry: StaticRegistry = {}
+let registry: StaticRegistry = {}
 
+/**
+ * Assigns static registry dependencies to variable given a valid path
+ *
+ * @param path
+ * @returns
+ */
 const initializeRegistry = (path: string | undefined): void => {
-  if (path == undefined || !fs.existsSync(path)) {
+  if (path === undefined || !fs.existsSync(path)) {
     throw new Error('No or invalid path to static registry was provided. Please try again')
   }
-  const rawdata = fs.readFileSync(path, 'utf8')
-  Object.assign(registry, JSON.parse(rawdata))
+  const data = JSON.parse(fs.readFileSync(path, 'utf8'))
+  registry = { ...registry, ...data }
 }
-
+/**
+ * Fetches the version in static registry.
+ *
+ * @param packageName
+ * @param currentVersion
+ * @param options
+ * @returns A promise that fulfills to string value or null
+ */
 export const latest: GetVersion = (packageName: string, currentVersion: Version, options: Options = {}) => {
-  console.log('foo ', options.registry)
-  console.log(registry)
-  if (Object.keys(registry).length == 0) {
+  if (Object.keys(registry).length === 0) {
     initializeRegistry(options.registry)
   }
   return Promise.resolve(registry[packageName] || null)
