9477ed75a268e2f9ec5d75551844166f8d87e21a
Raine Revere <raine@cybersemics.org>
2022-11-11 14:35:44 +0000

    Bump pacote → installed-package-contents (#1207).

--- .ncurc.js
+++ .ncurc.js
@@ -13,10 +13,5 @@ module.exports = {
     // Working upgrade is in branch 'hosted-git-info', but hold off on merging due to node engine requirements: ^14.17.0 || ^16.13.0 || >=18.0.0
     // https://github.com/npm/hosted-git-info/releases/tag/v6.0.0
     'hosted-git-info',
-    // Broken in v15.0.1
-    // Dependency installed-package-contents broken in v2.0.0
-    // https://github.com/raineorshine/npm-check-updates/issues/1207
-    // https://github.com/npm/installed-package-contents/issues/24
-    'pacote',
   ],
 }
