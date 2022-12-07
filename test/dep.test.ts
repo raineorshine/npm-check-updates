837396668def133e26dee928479358e863c1af9d
Raine Revere <raine@cybersemics.org>
2022-12-07 13:16:10 +0000

    Refactor dep tests.

--- test/dep.test.ts
+++ test/dep.test.ts
@@ -1,8 +1,6 @@
 import chai from 'chai'
 import chaiAsPromised from 'chai-as-promised'
 import chaiString from 'chai-string'
-import fs from 'fs/promises'
-import path from 'path'
 import * as ncu from '../src/'
 import stubNpmView from './helpers/stubNpmView'
 
@@ -12,16 +10,26 @@ chai.use(chaiString)
 
 process.env.NCU_TESTS = 'true'
 
+const packageData = JSON.stringify({
+  dependencies: {
+    'ncu-test-v2': '0.1.0',
+  },
+  devDependencies: {
+    'ncu-test-tag': '0.1.0',
+  },
+  peerDependencies: {
+    'ncu-test-10': '0.1.0',
+  },
+})
+
 it('do not upgrade peerDependencies by default', async () => {
   const stub = stubNpmView('99.9.9')
 
-  const upgraded = await ncu.run({
-    packageData: await fs.readFile(path.join(__dirname, 'test-data/ncu/package-dep.json'), 'utf-8'),
-  })
+  const upgraded = await ncu.run({ packageData })
 
-  upgraded!.should.have.property('express')
-  upgraded!.should.have.property('chalk')
-  upgraded!.should.not.have.property('mocha')
+  upgraded!.should.have.property('ncu-test-v2')
+  upgraded!.should.have.property('ncu-test-tag')
+  upgraded!.should.not.have.property('ncu-test-10')
 
   stub.restore()
 })
@@ -29,25 +37,19 @@ it('do not upgrade peerDependencies by default', async () => {
 it('only upgrade devDependencies with --dep dev', async () => {
   const stub = stubNpmView('99.9.9')
 
-  const upgraded = await ncu.run({
-    packageData: await fs.readFile(path.join(__dirname, 'test-data/ncu/package-dep.json'), 'utf-8'),
-    dep: 'dev',
-  })
+  const upgraded = await ncu.run({ packageData, dep: 'dev' })
 
-  upgraded!.should.not.have.property('express')
-  upgraded!.should.have.property('chalk')
-  upgraded!.should.not.have.property('mocha')
+  upgraded!.should.not.have.property('ncu-test-v2')
+  upgraded!.should.have.property('ncu-test-tag')
+  upgraded!.should.not.have.property('ncu-test-10')
 
   stub.restore()
 })
 
 it('only upgrade devDependencies and peerDependencies with --dep dev,peer', async () => {
-  const upgraded = await ncu.run({
-    packageData: await fs.readFile(path.join(__dirname, 'test-data/ncu/package-dep.json'), 'utf-8'),
-    dep: 'dev,peer',
-  })
+  const upgraded = await ncu.run({ packageData, dep: 'dev,peer' })
 
-  upgraded!.should.not.have.property('express')
-  upgraded!.should.have.property('chalk')
-  upgraded!.should.have.property('mocha')
+  upgraded!.should.not.have.property('ncu-test-v2')
+  upgraded!.should.have.property('ncu-test-tag')
+  upgraded!.should.have.property('ncu-test-10')
 })
