a66773d0660911baea067cdb75961fd28665813f
Frank Harrison <frank@doublethefish.com>
2022-12-09 22:47:53 +0000

    feat(use-pkg-metadata): de-indents getWorkspacePackages()

--- src/lib/getAllPackages.ts
+++ src/lib/getAllPackages.ts
@@ -21,82 +21,81 @@ async function getWorkspacePackages(
   rootPackageFile: string,
   cwd: string,
 ): Promise<[string[], string[]]> {
+  // workspaces
+  if (!(options.workspaces || options.workspace?.length)) {
+    return [[], []]
+  }
+
+  // use silent, otherwise there will be a duplicate "Checking" message
+  const [pkgData] = await findPackage({ ...options, packageFile: rootPackageFile, loglevel: 'silent' })
+  const rootPkg: PackageFile = typeof pkgData === 'string' ? JSON.parse(pkgData) : pkgData
+
+  const workspaces = Array.isArray(rootPkg.workspaces) ? rootPkg.workspaces : rootPkg.workspaces?.packages
+
+  if (!workspaces) {
+    programError(
+      options,
+      chalk.red(
+        `workspaces property missing from package.json. --workspace${
+          options.workspaces ? 's' : ''
+        } only works when you specify a "workspaces" property in your package.json.`,
+      ),
+    )
+  }
+
+  // build a glob from the workspaces
+  // FIXME: the following workspaces check is redundant
+  /* c8 ignore next */
+  const workspacePackageGlob: string[] = (workspaces || []).map(workspace =>
+    path
+      .join(cwd, workspace, 'package.json')
+      // convert Windows path to *nix path for globby
+      .replace(/\\/g, '/'),
+  )
+
+  // e.g. [packages/a/package.json, ...]
+  const allWorkspacePackageFilepaths: string[] = [
+    ...globby.sync(workspacePackageGlob, {
+      ignore: ['**/node_modules/**'],
+    }),
+  ]
+
+  // Get the package names from the package files.
+  // If a package does not have a name, use the folder name.
+  // These will be used to filter out local workspace packages so they are not fetched from the registry.
+  const allWorkspacePackageInfos: PackageInfo[] = [
+    ...(await Promise.all(
+      allWorkspacePackageFilepaths.map(async (filepath: string): Promise<PackageInfo> => {
+        const info: PackageInfo = await loadPackageInfoFromFile(filepath)
+        info.name = info.pkg.name || filepath.split('/').slice(-2)[0]
+        return info
+      }),
+    )),
+  ]
+
   // Workspace package names
   // These will be used to filter out local workspace packages so they are not fetched from the registry.
-  let workspacePackageNames: string[] = []
-  let workspacePackageFilepaths: string[] = []
+  const workspacePackageNames: string[] = allWorkspacePackageInfos.map(
+    (packageInfo: PackageInfo): string => packageInfo.name || '',
+  )
 
-  // workspaces
-  if (options.workspaces || options.workspace?.length) {
-    // use silent, otherwise there will be a duplicate "Checking" message
-    const [pkgData] = await findPackage({ ...options, packageFile: rootPackageFile, loglevel: 'silent' })
-    const rootPkg: PackageFile = typeof pkgData === 'string' ? JSON.parse(pkgData) : pkgData
-
-    const workspaces = Array.isArray(rootPkg.workspaces) ? rootPkg.workspaces : rootPkg.workspaces?.packages
-
-    if (!workspaces) {
-      programError(
-        options,
-        chalk.red(
-          `workspaces property missing from package.json. --workspace${
-            options.workspaces ? 's' : ''
-          } only works when you specify a "workspaces" property in your package.json.`,
-        ),
-      )
-    }
-
-    // build a glob from the workspaces
-    // FIXME: the following workspaces check is redundant
-    /* c8 ignore next */
-    const workspacePackageGlob: string[] = (workspaces || []).map(workspace =>
-      path
-        .join(cwd, workspace, 'package.json')
-        // convert Windows path to *nix path for globby
-        .replace(/\\/g, '/'),
-    )
-
-    // e.g. [packages/a/package.json, ...]
-    const allWorkspacePackageFilepaths: string[] = [
-      ...globby.sync(workspacePackageGlob, {
-        ignore: ['**/node_modules/**'],
-      }),
-    ]
-
-    // Get the package names from the package files.
-    // If a package does not have a name, use the folder name.
-    // These will be used to filter out local workspace packages so they are not fetched from the registry.
-    const allWorkspacePackageInfos: PackageInfo[] = [
-      ...(await Promise.all(
-        allWorkspacePackageFilepaths.map(async (filepath: string): Promise<PackageInfo> => {
-          const info: PackageInfo = await loadPackageInfoFromFile(filepath)
-          info.name = info.pkg.name || filepath.split('/').slice(-2)[0]
-          return info
-        }),
-      )),
-    ]
-
-    // Workspace package names
-    // These will be used to filter out local workspace packages so they are not fetched from the registry.
-    workspacePackageNames = allWorkspacePackageInfos.map((packageInfo: PackageInfo): string => packageInfo.name || '')
-
-    // add workspace packages
-    workspacePackageFilepaths = options.workspaces
-      ? // --workspaces
-        allWorkspacePackageFilepaths
-      : // --workspace
-        allWorkspacePackageFilepaths.filter(pkgFilepath =>
+  // add workspace packages
+  const workspacePackageFilepaths: string[] = options.workspaces
+    ? // --workspaces
+      allWorkspacePackageFilepaths
+    : // --workspace
+      allWorkspacePackageFilepaths.filter(pkgFilepath =>
+        /* ignore coverage on optional-chaining */
+        /* c8 ignore next */
+        options.workspace?.some(workspace =>
           /* ignore coverage on optional-chaining */
           /* c8 ignore next */
-          options.workspace?.some(workspace =>
-            /* ignore coverage on optional-chaining */
-            /* c8 ignore next */
-            workspaces?.some(
-              workspacePattern =>
-                pkgFilepath === path.join(cwd, path.dirname(workspacePattern), workspace, defaultPackageFilename),
-            ),
+          workspaces?.some(
+            workspacePattern =>
+              pkgFilepath === path.join(cwd, path.dirname(workspacePattern), workspace, defaultPackageFilename),
           ),
-        )
-  }
+        ),
+      )
   return [workspacePackageFilepaths, workspacePackageNames]
 }
 
