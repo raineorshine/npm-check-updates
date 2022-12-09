9889083a79b0f11dc18127e6cda3addc7bf82710
Raine Revere <raine@cybersemics.org>
2022-12-09 14:43:26 +0000

    Make Packument.time optional since it only exists with fullMetadata.

--- src/types/Packument.ts
+++ src/types/Packument.ts
@@ -8,7 +8,8 @@ export interface Packument {
   engines: {
     node: string
   }
-  time: Index<string>
+  // fullMetadata only
+  time?: Index<string>
   version: Version
   versions: Packument[]
 }
