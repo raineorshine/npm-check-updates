7d0d266d441661d26b41bd0969607a712a77ac26
Frank Harrison <frank@doublethefish.com>
2022-12-11 17:17:23 +0000

    chore(type): specify that programError() never returns, aiding typing
    This extra type spec means we don't have to add 'undefined' to functions
    that don't explicitly return when calling programError()

--- src/lib/programError.ts
+++ src/lib/programError.ts
@@ -2,7 +2,7 @@ import { print } from '../lib/logging'
 import { Options } from '../types/Options'
 
 /** Print an error. Exit the process if in CLI mode. */
-function programError(options: Options, message: string) {
+function programError(options: Options, message: string): never {
   if (options.cli) {
     print(options, message, null, 'error')
     process.exit(1)
