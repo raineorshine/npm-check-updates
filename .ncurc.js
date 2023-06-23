fabef5b0c98660b3b72890c64321a2357fa21439
Raine Revere <raine@cybersemics.org>
2023-06-23 18:03:17 +0000

    Bump dependencies

--- .ncurc.js
+++ .ncurc.js
@@ -8,6 +8,7 @@ module.exports = {
     'globby',
     'p-map',
     'remote-git-tags',
+    'untildify',
     // Broken in v6.0.0
     // Working upgrade is in branch 'hosted-git-info', but hold off on merging due to node engine requirements: ^14.17.0 || ^16.13.0 || >=18.0.0
     // https://github.com/npm/hosted-git-info/releases/tag/v6.0.0
