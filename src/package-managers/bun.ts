d3df52ad7592ee62e768df4cbe19137f2800a5df
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2023-08-16 10:14:04 +0700

    feat(package-managers): implement bun

--- src/package-managers/bun.ts
+++ src/package-managers/bun.ts
@@ -1,1 +1,42 @@
-// TODO: Initialize bun package manager spawner
+import spawn from 'spawn-please'
+import { Index } from '../types/IndexType'
+import { NpmOptions } from '../types/NpmOptions'
+import { latest as npmLatest, list as npmList } from './npm'
+
+/**
+ * (Bun) Fetches the list of all installed packages.
+ */
+export const list = npmList
+
+/**
+ * (Bun) Fetches the latest version of a package.
+ */
+export const latest = npmLatest
+
+/**
+ * Spawn bun.
+ *
+ * @param args
+ * @param [bunOptions={}]
+ * @param [spawnOptions={}]
+ * @returns
+ */
+async function spawnBun(
+  args: string | string[],
+  bunOptions: NpmOptions = {},
+  spawnOptions: Index<any> = {},
+): Promise<any> {
+  // Bun not yet supported on Windows.
+  // @see https://github.com/oven-sh/bun/issues/43
+  if (process.platform === 'win32') {
+    throw new Error('Bun not yet supported on Windows')
+  }
+
+  const cmd = 'bun'
+  args = Array.isArray(args) ? args : [args]
+
+  const fullArgs = args.concat('--global', bunOptions.prefix ? `--prefix=${bunOptions.prefix}` : [])
+  return spawn(cmd, fullArgs, spawnOptions)
+}
+
+export default spawnBun
--- test/package-managers/bun/index.test.ts
+++ test/package-managers/bun/index.test.ts
@@ -1,60 +1,18 @@
 import chai from 'chai'
 import chaiAsPromised from 'chai-as-promised'
-import chaiString from 'chai-string'
-import fs from 'fs/promises'
-import jph from 'json-parse-helpfulerror'
-import os from 'os'
-import path from 'path'
-import spawn from 'spawn-please'
+import * as bun from '../../../src/package-managers/bun'
 
 chai.should()
 chai.use(chaiAsPromised)
-chai.use(chaiString)
 
-process.env.NCU_TESTS = 'true'
-
-const bin = path.join(__dirname, '../../../build/src/bin/cli.js')
-
-describe('bun', async function () {
-  it('handle import map', async () => {
-    const tempDir = await fs.mkdtemp(path.join(os.tmpdir(), 'npm-check-updates-'))
-    const pkgFile = path.join(tempDir, 'bun.lockb')
-    const pkg = {
-      imports: {
-        'ncu-test-v2': 'npm:ncu-test-v2@1.0.0',
-      },
-    }
-    await fs.writeFile(pkgFile, JSON.stringify(pkg))
-    try {
-      const pkgData = await spawn(
-        'node',
-        [bin, '--jsonUpgraded', '--packageManager', 'bun', '--packageFile', pkgFile],
-        undefined,
-      )
-      const pkg = jph.parse(pkgData)
-      pkg.should.have.property('ncu-test-v2')
-    } finally {
-      await fs.rm(tempDir, { recursive: true, force: true })
-    }
+describe('bun', function () {
+  it('list', async () => {
+    const versionObject = await bun.list({ cwd: __dirname })
+    versionObject.should.have.property('express')
   })
 
-  it('auto detect bun.lockb', async () => {
-    const tempDir = await fs.mkdtemp(path.join(os.tmpdir(), 'npm-check-updates-'))
-    const pkgFile = path.join(tempDir, 'bun.lockb')
-    const pkg = {
-      imports: {
-        'ncu-test-v2': 'npm:ncu-test-v2@1.0.0',
-      },
-    }
-    await fs.writeFile(pkgFile, JSON.stringify(pkg))
-    try {
-      const pkgData = await spawn('node', [bin, '--jsonUpgraded'], undefined, {
-        cwd: tempDir,
-      })
-      const pkg = jph.parse(pkgData)
-      pkg.should.have.property('ncu-test-v2')
-    } finally {
-      await fs.rm(tempDir, { recursive: true, force: true })
-    }
+  it('latest', async () => {
+    const { version } = await bun.latest('express', '', { cwd: __dirname })
+    parseInt(version!, 10).should.be.above(1)
   })
 })
