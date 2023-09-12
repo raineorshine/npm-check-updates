15775ad06155315adf6764010d3d9ec3582ab0fe
Raine Revere <raine@cybersemics.org>
2023-09-12 15:42:28 +0000

    Implement bun.list, add targets, and fix --global.

--- src/package-managers/bun.ts
+++ src/package-managers/bun.ts
@@ -1,43 +1,68 @@
+import { Options } from 'pacote'
 import spawn from 'spawn-please'
+import keyValueBy from '../lib/keyValueBy'
 import programError from '../lib/programError'
 import { Index } from '../types/IndexType'
 import { NpmOptions } from '../types/NpmOptions'
-import { latest as npmLatest, list as npmList } from './npm'
+import * as npm from './npm'
 
-/**
- * (Bun) Fetches the list of all installed packages.
- */
-export const list = npmList
-
-/**
- * (Bun) Fetches the latest version of a package.
- */
-export const latest = npmLatest
-
-/**
- * Spawn bun.
- *
- * @param args
- * @param [bunOptions={}]
- * @param [spawnOptions={}]
- * @returns
- */
+/** Spawn bun. */
 async function spawnBun(
   args: string | string[],
-  bunOptions: NpmOptions = {},
+  npmOptions: NpmOptions = {},
   spawnOptions: Index<any> = {},
-): Promise<any> {
+): Promise<string> {
   // Bun not yet supported on Windows.
   // @see https://github.com/oven-sh/bun/issues/43
   if (process.platform === 'win32') {
-    programError(bunOptions, 'Bun not yet supported on Windows')
+    programError(npmOptions, 'Bun not yet supported on Windows')
   }
 
-  const cmd = 'bun'
   args = Array.isArray(args) ? args : [args]
 
-  const fullArgs = args.concat('--global', bunOptions.prefix ? `--prefix=${bunOptions.prefix}` : [])
-  return spawn(cmd, fullArgs, spawnOptions)
+  const fullArgs = [
+    ...args,
+    ...(npmOptions.prefix ? `--prefix=${npmOptions.prefix}` : []),
+    ...(npmOptions.location === 'global' ? ['--global'] : []),
+  ]
+
+  return spawn('bun', fullArgs, spawnOptions)
 }
+/**
+ * (Bun) Fetches the list of all installed packages.
+ */
+export const list = async (options: Options = {}): Promise<Index<string | undefined>> => {
+  const stdout = await spawnBun(
+    ['pm', 'ls'],
+    {
+      ...(options.global ? { location: 'global' } : null),
+      ...(options.prefix ? { prefix: options.prefix } : null),
+    },
+    {
+      ...(options.cwd ? { cwd: options.cwd } : null),
+      rejectOnError: false,
+    },
+  )
+
+  // parse the output of `bun pm ls` into an object { [name]: version }.
+  const lines = stdout.split('\n')
+  const dependencies = keyValueBy(lines, line => {
+    const match = line.match(/.* (.*?)@(.+)/)
+    if (match) {
+      const [, name, version] = match
+      return { [name]: version }
+    }
+    return null
+  })
+
+  return dependencies
+}
+
+export const greatest = npm.greatest
+export const latest = npm.latest
+export const newest = npm.newest
+export const semver = npm.semver
+export const minor = npm.minor
+export const patch = npm.patch
 
 export default spawnBun
--- test/package-managers/bun/index.test.ts
+++ test/package-managers/bun/index.test.ts
@@ -1,6 +1,7 @@
 import chai from 'chai'
 import chaiAsPromised from 'chai-as-promised'
 import chaiString from 'chai-string'
+import os from 'os'
 import * as bun from '../../../src/package-managers/bun'
 
 chai.should()
@@ -9,14 +10,17 @@ chai.use(chaiString)
 
 process.env.NCU_TESTS = 'true'
 
-describe('bun', function () {
-  it('list', async () => {
-    const versionObject = await bun.list({ cwd: __dirname })
-    versionObject.should.have.property('express')
-  })
+// Bun not yet supported on Windows
+if (os.platform() !== 'win32') {
+  describe('bun', function () {
+    it('list', async () => {
+      const result = await bun.list({ cwd: __dirname })
+      result.should.have.property('ncu-test-v2')
+    })
 
-  it('latest', async () => {
-    const { version } = await bun.latest('express', '', { cwd: __dirname })
-    parseInt(version!, 10).should.be.above(1)
+    it('latest', async () => {
+      const { version } = await bun.latest('ncu-test-v2', '1.0.0', { cwd: __dirname })
+      version!.should.equal('2.0.0')
+    })
   })
-})
+}
