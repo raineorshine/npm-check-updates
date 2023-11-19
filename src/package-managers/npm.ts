c513a84806f81db3161df67d44a93c0bfeddaa2f
Raine Revere <raine@cybersemics.org>
2023-11-19 20:42:13 +0000

    Fix pacote.manifest for real.

--- src/package-managers/npm.ts
+++ src/package-managers/npm.ts
@@ -451,7 +451,7 @@ async function viewMany(
   try {
     // when just fetching the dist-tag, use pacote.manifest instead of pacote.packument for a much smaller payload
     if (fieldsExtended.length === 1 && fieldsExtended[0].startsWith('dist-tags.')) {
-      const [, tag] = fieldsExtended[0].split('dist-tags')
+      const [, tag] = fieldsExtended[0].split('dist-tags.')
       const manifest = await pacote.manifest(packageName, npmConfigMerged)
       result = {
         [`dist-tags.${tag}`]: {
