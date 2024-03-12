5edd56d731350551d51f6f33202ca42c82de32bb
Raine Revere <raine@cybersemics.org>
2024-03-12 22:09:49 +0000

    Bump dependencies

--- .ncurc.js
+++ .ncurc.js
@@ -1,6 +1,11 @@
 module.exports = {
   format: 'group',
   reject: [
+    // TODO: major version upgrades
+    'chai',
+    'commander',
+    '@typescript-eslint/eslint-plugin',
+    '@typescript-eslint/parser',
     // ESM only modules
     // https://github.com/microsoft/TypeScript/issues/46452
     'find-up',
@@ -12,10 +17,6 @@ module.exports = {
     // Working upgrade is in branch 'hosted-git-info', but hold off on merging due to node engine requirements: ^14.17.0 || ^16.13.0 || >=18.0.0
     // https://github.com/npm/hosted-git-info/releases/tag/v6.0.0
     'hosted-git-info',
-    // Removed support for node v14 in v0.35.0
-    'makdownlint-cli',
-    // manually keep in alignment with pacote's version of make-fetch-happen
-    'make-fetch-happen',
     // major changes required to upgrade to v3
     'spawn-please',
     // v0.60.0 breaks cli option description output
