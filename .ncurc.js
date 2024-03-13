c858951b35a112481520076e65a7022045259861
Raine Revere <raine@cybersemics.org>
2024-03-13 23:40:36 +0000

    Bump dependencies that require node >= 18.

--- .ncurc.js
+++ .ncurc.js
@@ -12,10 +12,5 @@ module.exports = {
     // v0.60.0 breaks cli option description output
     // https://github.com/YousefED/typescript-json-schema/issues/568
     'typescript-json-schema',
-    // node >= 18
-    'commander', // https://github.com/tj/commander.js/blob/master/CHANGELOG.md
-    'update-notifier',
-    '@typescript-eslint/eslint-plugin',
-    '@typescript-eslint/parser',
   ],
 }
