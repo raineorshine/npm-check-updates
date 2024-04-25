bc18c925fc8670ba6be3e0784cded8ec9b1bcdb6
Raine Revere <raine@cybersemics.org>
2024-04-25 23:53:37 +0000

    Move bun install and tests to test:bun script.
    Bun install fails on some Linux distros, so it needs to be moved to a separate install and test script that is optional. It is still run in the GitHub workflow.

--- test/bun-install.sh
+++ test/bun-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Install bun if not installed.
-# Must be run in a prepare script instead of devDependencies to avoid npm install failing on Windows.
-bun -v &> /dev/null
+# Cannot be added to devDependencies as bun currently does not work on Linux.
+bun -v &>/dev/null
 BUN_EXISTS="$?"
 
 if [ $BUN_EXISTS -ne 0 ]; then
