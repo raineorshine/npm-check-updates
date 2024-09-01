6fb4285d322476500293bac84a26742dcd777434
rbnayax <92677232+rbnayax@users.noreply.github.com>
2024-09-01 18:46:31 +0300

    fix: Do not upgrade if peerDependencies are not met (#1421)

--- test/peer.test.ts
+++ test/peer.test.ts
@@ -1,5 +1,8 @@
+import path from 'path'
 import ncu from '../src/'
+import { Packument } from '../src/types/Packument'
 import chaiSetup from './helpers/chaiSetup'
+import stubVersions from './helpers/stubVersions'
 
 chaiSetup()
 
@@ -73,5 +76,117 @@ describe('peer dependencies', function () {
       },
     })
     upgrades!.should.deep.equal({})
+  })
+
+  it('ignores if post upgrade peers are unmet', async () => {
+    const stub = stubVersions({
+      '@vitest/ui': {
+        version: '1.6.0',
+        versions: {
+          '1.3.1': {
+            version: '1.3.1',
+          } as Packument,
+          '1.6.0': {
+            version: '1.6.0',
+          } as Packument,
+        },
+      },
+      vitest: {
+        version: '1.6.0',
+        versions: {
+          '1.3.1': {
+            version: '1.3.1',
+          } as Packument,
+          '1.6.0': {
+            version: '1.6.0',
+          } as Packument,
+        },
+      },
+      eslint: {
+        version: '9.0.0',
+        versions: {
+          '8.57.0': {
+            version: '8.57.0',
+          } as Packument,
+          '9.0.0': {
+            version: '9.0.0',
+          } as Packument,
+        },
+      },
+      'eslint-plugin-import': {
+        version: '2.29.1',
+        versions: {
+          '2.29.1': {
+            version: '2.29.1',
+          } as Packument,
+        },
+      },
+      'eslint-plugin-unused-imports': {
+        version: '4.0.0',
+        versions: {
+          '4.0.0': {
+            version: '4.0.0',
+          } as Packument,
+          '3.0.0': {
+            version: '3.0.0',
+          } as Packument,
+        },
+      },
+    })
+    const cwd = path.join(__dirname, 'test-data/peer-post-upgrade/')
+    const upgrades = await ncu({
+      cwd,
+      peer: true,
+      target: packageName => {
+        return packageName === 'eslint-plugin-unused-imports' ? 'greatest' : 'minor'
+      },
+    })
+    upgrades!.should.have.all.keys('@vitest/ui', 'vitest')
+    stub.restore()
+  })
+
+  it('ignores if post upgrade peers are unmet - no upgrades', async () => {
+    const stub = stubVersions({
+      eslint: {
+        version: '9.0.0',
+        versions: {
+          '8.57.0': {
+            version: '8.57.0',
+          } as Packument,
+          '9.0.0': {
+            version: '9.0.0',
+          } as Packument,
+        },
+      },
+      'eslint-plugin-import': {
+        version: '2.29.1',
+        versions: {
+          '2.29.1': {
+            version: '2.29.1',
+          } as Packument,
+        },
+      },
+      'eslint-plugin-unused-imports': {
+        version: '4.0.0',
+        versions: {
+          '4.0.0': {
+            version: '4.0.0',
+          } as Packument,
+          '3.0.0': {
+            version: '3.0.0',
+          } as Packument,
+        },
+      },
+    })
+    const cwd = path.join(__dirname, 'test-data/peer-post-upgrade-no-upgrades/')
+    const upgrades = await ncu({
+      cwd,
+      peer: true,
+      target: packageName => {
+        return packageName === 'eslint-plugin-unused-imports' ? 'greatest' : 'minor'
+      },
+    })
+    upgrades!.should.deep.equal({})
+    stub.restore()
   })
 })
