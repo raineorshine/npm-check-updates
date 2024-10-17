5243f6dceeb5c97cac592311aff5050594dde6d0
Raine Revere <raine@cybersemics.org>
2024-10-17 07:07:55 +0000

    Bump dependencies

--- .ncurc.js
+++ .ncurc.js
@@ -2,11 +2,15 @@ module.exports = {
   format: 'group',
   reject: [
     // breaking
+    '@types/chai-as-promised',
+    '@types/node',
     'chai-as-promised',
     'eslint',
     'eslint-plugin-n',
     'eslint-plugin-promise',
     // esm only modules
+    '@types/chai',
+    '@types/remote-git-tags',
     'camelcase',
     'find-up',
     'chai',
