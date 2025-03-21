raineorshine/npm-check-updates

commit 3b12e6a67f3ee7f549e92b0d2f8f4b3de81c48a6

Enable bun on Windows (#1401)

src/index.ts
@@ -139,7 +139,7 @@ const install = async (
 
     pkgsNormalized.forEach(async pkgFile => {
       const packageManager = await getPackageManagerForInstall(options, pkgFile)
-      const cmd = packageManager + (process.platform === 'win32' ? '.cmd' : '')
+      const cmd = packageManager + (process.platform === 'win32' && packageManager !== 'bun' ? '.cmd' : '')
       const cwd = options.cwd || path.resolve(pkgFile, '..')
       let stdout = ''
       try {

src/package-managers/bun.ts
@@ -2,7 +2,6 @@ import { Options } from 'pacote'
 import path from 'path'
 import spawn from 'spawn-please'
 import keyValueBy from '../lib/keyValueBy'
-import programError from '../lib/programError'
 import { Index } from '../types/IndexType'
 import { NpmOptions } from '../types/NpmOptions'
 import * as npm from './npm'
@@ -13,12 +12,6 @@ async function spawnBun(
   npmOptions: NpmOptions = {},
   spawnOptions: Index<any> = {},
 ): Promise<string> {
-  // Bun not yet supported on Windows.
-  // @see https://github.com/oven-sh/bun/issues/43
-  if (process.platform === 'win32') {
-    programError(npmOptions, 'Bun not yet supported on Windows')
-  }
-
   const fullArgs = [
     ...(npmOptions.prefix ? [`--prefix=${npmOptions.prefix}`] : []),
     ...(npmOptions.location === 'global' ? ['--global'] : []),

test/bun-setup.sh
@@ -1,12 +1,0 @@
-# Install bun if not installed.
-# Must be run in a prepare script instead of devDependencies to avoid npm install failing on Windows.
-bun -v &> /dev/null
-BUN_EXISTS="$?"
-
-if [ $BUN_EXISTS -ne 0 ]; then
-  npm install -g bun
-fi
-
-# Always return success, even if the install script fails.
-# Windows is expected to fail and the bun tests will be skipped.
-exit 0

test/doctor.test.ts
@@ -456,7 +456,6 @@ else {
     testFail({ packageManager: 'yarn' })
   })
 
-  // Bun not yet supported on Windows
   // const describeSkipWindows = os.platform() === 'win32' ? describe.skip : describe
   // TODO: Works locally, but not in GitHub action.
   describe.skip('bun', () => {

test/package-managers/bun/index.test.ts
@@ -1,12 +1,9 @@
-import os from 'os'
 import * as bun from '../../../src/package-managers/bun'
 import chaiSetup from '../../helpers/chaiSetup'
 
 chaiSetup()
 
-// Bun not yet supported on Windows
-const describeSkipWindows = os.platform() === 'win32' ? describe.skip : describe
-describeSkipWindows('bun', function () {
+describe('bun', function () {
   it('list', async () => {
     const result = await bun.list({ cwd: __dirname })
     result.should.have.property('ncu-test-v2')

commit 94a874fd8e84048cd89612121a34b7dbfe6d91dc

Use bun instead of bun.cmd for autoinstall on Windows (#1400).
Possibly was an accidental regression in #1401, mistaken as an exclusion of bun rather than an exclusion of the .cmd extension.

src/index.ts
@@ -146,7 +146,8 @@ const install = async (
 
     for await (const pkgFile of pkgsNormalized) {
       const packageManager = await getPackageManagerForInstall(options, pkgFile)
-      const cmd = packageManager + (process.platform === 'win32' ? '.cmd' : '')
+      // npm, yarn, pnpm use .cmd on Windows, but bun does not
+      const cmd = packageManager + (process.platform === 'win32' && packageManager !== 'bun' ? '.cmd' : '')
       const cwd = options.cwd || path.resolve(pkgFile, '..')
       let stdout = ''
       try {