8cebd5dd7acf61693f2f70614e38710262815e24
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-08-04 14:03:05 -0400

    remove

--- src/lib/getPeerDependenciesFromRegistry.ts
+++ src/lib/getPeerDependenciesFromRegistry.ts
@@ -1,10 +1,8 @@
-import pMap from 'p-map'
 import ProgressBar from 'progress'
 import { Index } from '../types/IndexType'
 import { Options } from '../types/Options'
 import { Version } from '../types/Version'
 import getPackageManager from './getPackageManager'
-import keyValueBy from './keyValueBy'
 
 type CircularData =
   | {
@@ -54,38 +52,26 @@ async function getPeerDependenciesFromRegistry(packageMap: Index<Version>, optio
   const packageManager = getPackageManager(options, options.packageManager)
   if (!packageManager.getPeerDependencies) return {}
 
-  const packageList = Object.keys(packageMap)
-  const numItems = packageList.length
+  const numItems = Object.keys(packageMap).length
   let bar: ProgressBar
   if (!options.json && options.loglevel !== 'silent' && options.loglevel !== 'verbose' && numItems > 0) {
     bar = new ProgressBar('[:bar] :current/:total :percent', { total: numItems, width: 20 })
     bar.render()
   }
 
-  const peerDepsList = await pMap(
-    packageList,
-    async (pkg: string) => {
-      const version = packageMap[pkg]
-      const dep = await packageManager.getPeerDependencies!(pkg, version)
-      bar?.tick()
-      return {
-        [pkg]: dep,
-      }
-    },
-    { concurrency: options.concurrency },
-  )
-
-  const result = keyValueBy(peerDepsList, item => item)
-
-  // check for circular dependencies
-  for (const pkg of Object.keys(result)) {
-    const circularData = isCircularPeer(result, pkg)
-    if (circularData.isCircular) {
-      delete result[pkg][circularData.offendingPackage]
+  return Object.entries(packageMap).reduce(async (accumPromise, [pkg, version]) => {
+    const dep = await packageManager.getPeerDependencies!(pkg, version)
+    if (bar) {
+      bar.tick()
     }
-  }
-
-  return result
+    const accum = await accumPromise
+    const newAcc: Index<Index<string>> = { ...accum, [pkg]: dep }
+    const circularData = isCircularPeer(newAcc, pkg)
+    if (circularData.isCircular) {
+      delete newAcc[pkg][circularData.offendingPackage]
+    }
+    return newAcc
+  }, Promise.resolve<Index<Index<string>>>({}))
 }
 
 export default getPeerDependenciesFromRegistry
