8c573cd8fe3b0b8f197b43abc0e28348809eb36a
James <5511220+Zamiell@users.noreply.github.com>
2025-09-18 16:00:22 -0400

    fix: package manager detection (#1545)

--- src/lib/findLockfile.ts
+++ src/lib/findLockfile.ts
@@ -1,4 +1,5 @@
 import fs from 'fs/promises'
+import os from 'os'
 import path from 'path'
 import { Options } from '../types/Options'
 
@@ -13,7 +14,7 @@ const lockFileNames = [
 ]
 
 /**
- * Goes up the filesystem tree until it finds a package-lock.json, yarn.lock, pnpm-lock.yaml, deno.json, deno.jsonc, or bun.lockb file.
+ * Goes up the filesystem tree until it finds a lock file. (e.g. "package-lock.json", "yarn.lock", etc.)
  *
  * @param readdir This is only a parameter so that it can be used in tests.
  * @returns The path of the directory that contains the lockfile and the
@@ -24,10 +25,15 @@ export default async function findLockfile(
   readdir: (_path: string) => Promise<string[]> = fs.readdir,
 ): Promise<{ directoryPath: string; filename: string } | null> {
   try {
+    // Get boundaries to stop searching.
+    const homeDir = os.homedir()
+    const tempDir = os.tmpdir()
+
     // 1. explicit cwd
     // 2. same directory as package file
     // 3. current directory
     let currentPath = options.cwd ? options.cwd : options.packageFile ? path.dirname(options.packageFile) : '.'
+    currentPath = path.resolve(currentPath)
 
     while (true) {
       const files = await readdir(currentPath)
@@ -39,7 +45,16 @@ export default async function findLockfile(
       }
 
       const pathParent = path.resolve(currentPath, '..')
-      if (pathParent === currentPath) break
+      if (
+        // Stop if we have reached the root of the file system.
+        pathParent === currentPath ||
+        // Stop if we have reached the root of a user's home directory.
+        pathParent === homeDir ||
+        // Stop if we have reached the root of the temporary directory.
+        pathParent === tempDir
+      ) {
+        break
+      }
 
       currentPath = pathParent
     }
