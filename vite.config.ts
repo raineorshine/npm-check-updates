9487df7f6e0dcb1f5df11cc37c1a43fe0811ce6d
onemen <tabmix.onemen@gmail.com>
2026-03-31 22:56:30 +0300

    upgrade to vite 8
    update vite config
    remove unused nodeExternals, we see ssr.noExternal=true so we don't want to exclude node modules from the bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,18 @@
-import { nodeExternals } from 'rollup-plugin-node-externals'
 import { defineConfig } from 'vite'
 import { analyzer } from 'vite-bundle-analyzer'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig(({ mode }) => ({
   plugins: [
-    {
-      name: 'log-imports',
-      resolveId(source, importer) {
-        console.log(`[resolve] ${source} <- ${importer}`)
-        return null // let Vite handle it normally
-      },
-    },
     dts({
       entryRoot: 'src',
-      rollupTypes: true,
       include: ['src'],
+      rollupTypes: true,
       insertTypesEntry: true,
     }),
-    nodeExternals(),
     ...(process.env.ANALYZER ? [analyzer()] : []),
   ],
   ssr: {
-    // bundle and treeshake everything
     noExternal: true,
   },
   build: {
@@ -32,13 +22,20 @@ export default defineConfig(({ mode }) => ({
         index: 'src/index.ts',
         cli: 'src/bin/cli.ts',
       },
-      // Essential for the 'type: module' migration
       formats: ['es'],
       fileName: (format, entryName) => `${entryName}.js`,
     },
     target: 'node20',
     outDir: 'build',
     sourcemap: true,
-    minify: mode === 'production' ? 'esbuild' : false,
+    minify: mode === 'production',
+    rollupOptions: {
+      output: {
+        // This removes the 'p-map-tgCadq-q.js' hash and uses a cleaner name
+        chunkFileNames: 'chunks/[name].js',
+        // Ensure the entry points stay exactly where you want them
+        entryFileNames: '[name].js',
+      },
+    },
   },
 }))
