40dca732c7a23a2f73aabecf3a1fc85212b3d812
Terminal Chai <terminalchai@gmail.com>
2026-06-04 01:07:08 +0530

    test: cover Windows spawn command fallback (#1771)
    Signed-off-by: terminalchai <terminalchai@gmail.com>

---
+++ test/spawnCommand.test.ts
@@ -0,0 +1,18 @@
+import { getSpawnCommands } from '../src/lib/spawnCommand'
+import chaiSetup from './helpers/chaiSetup'
+
+chaiSetup()
+
+describe('spawnCommand', () => {
+  it('prefers cmd shims on Windows and falls back to extensionless commands', () => {
+    getSpawnCommands('pnpm', 'win32').should.deep.equal(['pnpm.cmd', 'pnpm'])
+  })
+
+  it('does not use cmd shims on non-Windows platforms', () => {
+    getSpawnCommands('pnpm', 'linux').should.deep.equal(['pnpm'])
+  })
+
+  it('does not use cmd shims for bun on Windows', () => {
+    getSpawnCommands('bun', 'win32').should.deep.equal(['bun'])
+  })
+})
