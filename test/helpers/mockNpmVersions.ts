9625c18edb8a7e27b7679f75234f33f6dc1f3a92
XhmikosR <xhmikosr@gmail.com>
2026-06-25 18:15:45 +0300

    Dedupe mockNpmVersions fixture in tests

---
+++ test/helpers/mockNpmVersions.ts
@@ -0,0 +1,9 @@
+/** Mock registry versions used by the doctor tests. */
+const mockNpmVersions = {
+  emitter20: '2.0.0',
+  'ncu-test-return-version': '2.0.0',
+  'ncu-test-tag': '1.1.0',
+  'ncu-test-v2': '2.0.0',
+}
+
+export default mockNpmVersions
