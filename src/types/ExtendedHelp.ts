85623a2c1a001019ded45fa52014a95669d7d014
XhmikosR <xhmikosr@gmail.com>
2026-06-27 19:00:04 +0300

    tsconfig: enable verbatimModuleSyntax (#1860)

--- src/types/ExtendedHelp.ts
+++ src/types/ExtendedHelp.ts
@@ -1,4 +1,4 @@
 /** A function that renders extended help for an option. */
 type ExtendedHelp = string | ((options: { markdown?: boolean }) => string)
 
-export default ExtendedHelp
+export type { ExtendedHelp as default }
